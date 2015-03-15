83a92e7c08ea9b1297fccc40f6f4cac409d5dc2c
MWHanco <MWHanco@2355DQ4PK127V7.nam.ent.duke-energy.com>
2015-03-15 17:15:24 -0400

    Created two classes;  BuyerAgent and SellerAgent.  nothing in there except setup method that outputs a printline statement

---
+++ acbryan6_mwhancoc_p3/src/acbryan6_mwhancoc_p3/BuyerAgent.java
@@ -0,0 +1,13 @@
+package acbryan6_mwhancoc_p3;
+
+import jade.core.Agent;
+
+public class BuyerAgent extends Agent{
+	
+	protected void setup() {
+		 // Printout a welcome message
+		 System.out.println("Hello! Buyer-agent " + getAID().getName() + " is ready.");
+	 }
+
+
+}
---
+++ acbryan6_mwhancoc_p3/src/acbryan6_mwhancoc_p3/SellerAgent.java
@@ -0,0 +1,12 @@
+package acbryan6_mwhancoc_p3;
+
+import jade.core.Agent;
+
+public class SellerAgent extends Agent{
+	
+	protected void setup() {
+		 // Printout a welcome message
+		 System.out.println("Hello! Buyer-agent " + getAID().getName() + " is ready.");
+	 }
+
+}
