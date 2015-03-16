4b64588b4e7b2f9d3164c127b5b965ed764afdbf
MWHanco <MWHanco@2355DQ4PK127V7.nam.ent.duke-energy.com>
2015-03-15 20:38:11 -0400

    Added a hashtable in the SellerAgent to keep track of a catalogue.  Will use the catalogue for testing incoming requests for a a quote on an item sent from a buyer agent.

--- acbryan6_mwhancoc_p3/src/acbryan6_mwhancoc_p3/SellerAgent.java
+++ acbryan6_mwhancoc_p3/src/acbryan6_mwhancoc_p3/SellerAgent.java
@@ -1,10 +1,20 @@
 package acbryan6_mwhancoc_p3;
 
+import java.util.Hashtable;
+
 import jade.core.Agent;
 
 public class SellerAgent extends Agent{
 	
+	private Hashtable catalogue;
+	
 	protected void setup() {
+		
+		// Create the catalogue, hard code two items, just to test ACLMessages
+		 catalogue = new Hashtable();
+		 catalogue.put(1, 1.99);
+		 catalogue.put(2,  2.99);
+		
 		 // Printout a welcome message
 		 System.out.println("Hello! Seller-agent " + getAID().getName() + " is ready.");
 	 }
